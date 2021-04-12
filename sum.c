d0a942798748bc9996368e569732a2df55a60547
rix <rix0ztha@gmail.com>
2021-04-12 17:32:36 +0545

    *

--- sum.c
+++ sum.c
@@ -6,8 +6,6 @@ void main()
    printf("Enter 3 numbers\n");
    scanf("%d%d%d", &var1, &var2, &var3);
    sum = var1 + var2 + var3;
-   printf("Result is %d", var1&var2);
+   printf("Result is %d", sum );
 }
 
-//WAP to take feignheit as input from user and convert it onto Celcius
-//WAP to tkae input of 3 sides of a triangle as input and calculate area and display it

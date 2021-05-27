dba429760c41e23bec3b378a7b1d9392738c56d4
rix <rix0ztha@gmail.com>
2021-05-27 10:21:08 +0545

    *

--- tutorial-passbyaddress-and-array/7ii.c
+++ tutorial-passbyaddress-and-array/7ii.c
@@ -2,34 +2,40 @@
 // ii)Make different function to take input from userand to display
 // values.
 #include <stdio.h>
-int firstMatrix() {
-  int first[2][2];
+void takeInput(int matrix[2][2]);
+void output(int matrix[2][2]);
+
+int main(void) {
+  int a[2][2], b[2][2], c[2][2];
+  printf("Enter first matrix\n");
+  takeInput(a);
+  printf("\nEnter second matrix\n");
+  takeInput(b);
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 2; j++) {
-      printf("first[%d][%d]: ", i + 1, j + 1);
-      scanf("%d", &first[i][j]);
+      c[i][j] = a[i][j] + b[i][j];
     }
   }
-  return first;
+  printf("\nSum:\n");
+  output(c);
+  return 0;
 }
 
-int secMatrix() {
-  int sec[2][2];
+void takeInput(int matrix[2][2]) {
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 2; j++) {
-      printf("second[%d][%d]: ", i + 1, j + 1);
-      scanf("%d", &sec[i][j]);
+      printf("[%d][%d]: ", i, j);
+      scanf("%d", &matrix[i][j]);
     }
   }
-  return sec;
 }
-int main(void) {
-  int sum[2][2], first[2][2], sec[2][2];
+
+void output(int matrix[2][2]) {
   for (int i = 0; i < 2; i++) {
+    printf("[ ");
     for (int j = 0; j < 2; j++) {
-      printf("%d ", first[i][j] + sec[i][j]);
+      printf("%d ", matrix[i][j]);
     }
-    printf("\n");
+    printf("]\n");
   }
-  return 0;
 }

86ad638cc0441706220a62f51b58c0118d180c9a
Rix <35215363+rixyz@users.noreply.github.com>
2021-06-07 16:34:36 +0545

    Update 4.c

--- tutorial-struct/4.c
+++ tutorial-struct/4.c
@@ -1,5 +1,5 @@
-/* WAP to read serial_number,price,brand_name,model_numberof different 20
-// laptops using structure.*/
+// WAP to read serial_number,price,brand_name,model_numberof different 20
+// laptops using structure.
 #include <stdio.h>
 struct laptop_info {
   int serial_number;
@@ -9,17 +9,13 @@ struct laptop_info {
 };
 int main(void) {
   struct laptop_info laptop[20];
-  int i = 0;
-  for (i = 0; i < 20; i++) {
-    printf("a:");
+  for (int i = 0; i < 20; i++) {
+    printf("Enter Serial Number, Price, Brand Name and Model Number\n");
     scanf("%d", &laptop[i].serial_number);
-    printf("b:");
     scanf("%d", &laptop[i].price);
-    printf("c:");
     scanf("%s", laptop[i].brand_name);
-    printf("d:");
     scanf("%s", laptop[i].model_number);
-    printf("a:%d b:%d c:%s d:%s\n", laptop[i].serial_number, laptop[i].price,
+    printf("Serial Number:%d \nPrice:%d Brand Name:%s \nModel Number:%s\n", laptop[i].serial_number, laptop[i].price,
            laptop[i].brand_name, laptop[i].model_number);
   }
   return 0;
